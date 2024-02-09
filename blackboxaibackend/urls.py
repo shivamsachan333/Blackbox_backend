ecb8b8b0f3d90421e7ce3a0d828ad50d6eca024a
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-09 11:48:09 +0530

    Update urls.py

--- blackboxaibackend/urls.py
+++ blackboxaibackend/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('', include('backends.urls')),
 
 
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
