2112e6694913ee231aa724e89310fe68ebdb7ae0
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-09 11:48:42 +0530

    Update settings.py

--- blackboxaibackend/settings.py
+++ blackboxaibackend/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'blackboxaibackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates','static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
