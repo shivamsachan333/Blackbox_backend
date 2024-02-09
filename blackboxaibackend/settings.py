08917871b58c6537635472f81ea8f3e286b72a7d
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-09 11:31:28 +0530

    Update settings.py

--- blackboxaibackend/settings.py
+++ blackboxaibackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-idlv^&5vzrqe!yoh+*8nk2af6he&49lvi@(qkg6%o681oe=5mv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1',"*","https://trading.work.gd",'trading.work.gd','.vercel.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1',"*","https://trading.work.gd",'trading.work.gd','.vercel.app','now.sh']
 
 
 # Application definition
