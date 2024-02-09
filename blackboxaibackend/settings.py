a6fa48c1f562cef0129290be45f8d9b6c0efb5fc
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-09 11:32:35 +0530

    Update settings.py

--- blackboxaibackend/settings.py
+++ blackboxaibackend/settings.py
@@ -80,12 +80,23 @@ WSGI_APPLICATION = 'blackboxaibackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GzCkMixulXwWnIPYlkex',
+        'HOST': 'containers-us-west-92.railway.app',
+        'PORT': '5526',
     }
 }
+
 
 
 # Password validation
